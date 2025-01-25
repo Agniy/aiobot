9d092be0f9897c1bf3d1dbd0bc1c1552db812977
ishev <ilya@decard.me>
2025-01-25 13:36:10 +0300

    Add setting and img cmd - 2.

--- handlers/cmd_image.py
+++ handlers/cmd_image.py
@@ -1,8 +1,21 @@
+import aiofiles
 from aiogram import  types
-from aiogram.types import FSInputFile
+from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
 from config.settings import settings
 
 async def cmd_image(message: types.Message):
     image_path = settings.STATIC_DIR / "gus.jpg"
     image_file = FSInputFile(image_path)
-    await message.answer_photo(image_file)
+    await message.answer_photo(image_file,caption="Гусь лапчатый")
+
+    await message.answer_photo(
+        URLInputFile("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4wvHlghvqqzJfcrSo5ApaTgXSHteMyJHEQQ&s"),
+        caption="Принцесса"
+    )
+
+    with open(image_path, "rb") as f:
+        image_bytes = f.read()
+        await message.answer_photo(
+            BufferedInputFile(image_bytes, filename="gus.jpg"),
+            caption="Гусь лапчатый"
+        )
